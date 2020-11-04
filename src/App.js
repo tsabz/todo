7c07fdd023e60eaca1faf37715fee5e4987753db
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-11-04 15:16:21 -0500

    added todo list

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header>
           <h1>T's Todo List</h1>
         </header>
-        <Form setInputText={setInputText}/>
+        <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
         <TodoList/>
     </div>
   );
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Todo from './todo.js';
 
 const TodoList = () => {
     return (
         <div className="todo-container">
-        <ul className="todo-list"></ul>
+        <ul className="todo-list">
+            <Todo />
+        </ul>
       </div>
     )
 }
